c4a68647e292f5e60ba609f93d4208358f7653f9
g0mteng <gomteng03@naver.com>
2024-01-21 20:50:44 +0900

    null

---
+++ lib/view/CardPostView.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+class CardPostPage extends StatelessWidget {
+  const CardPostPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/view/QuizView.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+class QuizPage extends StatelessWidget {
+  const QuizPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
