7fbdd25f38385ca17c6a1ad8164965bdcfcf1cf6
g0mteng <gomteng03@naver.com>
2024-01-31 13:24:45 +0900

    add file struct

--- lib/LoginView.dart
+++ lib/LoginView.dart
@@ -1,40 +1,42 @@
 import 'package:flutter/material.dart';
-
 import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
-import 'package:social_login_app/login_platform.dart';
+import 'view/MainView.dart';
+import 'package:get/get.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
-  void loginKakao() async {
+  Future<bool> loginKakao() async {
     if (await isKakaoTalkInstalled()) {
       try {
         await UserApi.instance.loginWithKakaoTalk();
         print('로그인 성공 !');
+        return true;
       } catch (e) {
         print('로그인 실패 ${e}');
-
-        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우
-        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리
-        if (e is PlatformException && e.code == 'CANCELED') return;
+        return false;
       }
 
       try {
         await UserApi.instance.loginWithKakaoAccount();
         print('로그인 성공 !');
+        return true;
       } catch (e) {
         print('로그인 실패 ${e}');
+        return false;
       }
     } else {
       try {
         await UserApi.instance.loginWithKakaoAccount();
         print('로그인 성공 !');
+        return true;
       } catch (e) {
         print('로그인 실패 ${e}');
+        return false;
       }
     }
   }
@@ -57,9 +59,12 @@ class LoginPage extends StatelessWidget {
                 ),
                 SizedBox(height: 60),
                 InkWell(
-                  onTap: () {
+                  onTap: () async {
                     // 카카오 로그인
-                    loginKakao();
+                    bool islogin = await loginKakao();
+                    if (islogin) {
+                      Get.off(MainPage());
+                    }
                   },
                   child: Image.asset('assets/image/kakao_login.png'),
                 ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'view/HomeView.dart';
-import 'view/QuizView.dart';
-import 'view/CardPostView.dart';
-import 'view/WorkPostView.dart';
 import 'LoginView.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() {
   KakaoSdk.init(nativeAppKey: '34f7562a97bfec0487353991c1529ab6');
   runApp(GetMaterialApp(home: LoginPage()));
-}
-
-class Main extends StatelessWidget {
-  int _index = 0;
-
-  final List<Widget> _pages = [
-    HomePage(),
-    CardPostPage(),
-    WorkPostPage(),
-    QuizPage(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('bottomNavigator'),
-      ),
-      body: _pages[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _index,
-        items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'card'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'work'),
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'quiz'),
-        ],
-      ),
-    );
-  }
 }
