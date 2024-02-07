82348257ed6ceb9d04e5c92ce1f9dd49cf441186
Eunjin Oh <ibsuny3214@naver.com>
2024-02-08 06:40:33 +0900

    #2 feat: scrap을 제외한 모든 기능 구현

---
+++ lib/controller/cardpost_controller.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:get/get.dart';
+import 'dart:convert';
+import '../model/cardpost_model.dart';
+
+class CardPostController extends GetxController{
+  var Cards = CardPostModel().obs;
+
+  @override
+  void onInit(){
+    super.onInit();
+    fetchCardsFromLocalJson();
+  }
+  Future<void> fetchCardsFromLocalJson() async{
+    try {
+      final String response = await rootBundle.loadString(
+          'assets/test/cardpost.json');
+      final data = json.decode(response);
+      Cards.value = CardPostModel.fromJson(data);
+    } catch (e) {
+      // 오류 처리
+      print('JSON 파일 로드 또는 파싱 중 오류 발생: $e');
+    }
+  }
+}
+
