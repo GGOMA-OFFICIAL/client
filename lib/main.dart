fa7be297c07ac8f1f50d767c00529843d4bb0295
Eunjin Oh <ibsuny3214@naver.com>
2024-02-08 23:18:36 +0900

    #5 login page 삭제 (branch: NotLogin)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'LoginView.dart';
+import './view/MainView.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() {
   KakaoSdk.init(nativeAppKey: '34f7562a97bfec0487353991c1529ab6');
-  runApp(GetMaterialApp(home: LoginPage()));
+  runApp(GetMaterialApp(home: MainPage()));
 }
